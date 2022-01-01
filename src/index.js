0f1c53bce3a24e8aa5fb1e0c431e52c41d50e56a
THeikki <t9tohe00@students.oamk.fi>
2022-01-01 16:54:12 +0200

    up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom';
 //import React from 'react';
 import './index.css';
 import App from './App';
+import { StrictMode } from 'react/cjs/react.production.min';
 
 ReactDOM.render(
     <App />,
