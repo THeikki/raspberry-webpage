6317d2260aa8a8e9c834f1397250f8dd9b54aab0
THeikki <t9tohe00@students.oamk.fi>
2022-01-01 16:15:32 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+//import React from 'react';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
