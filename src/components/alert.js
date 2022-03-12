f38717c386fec84ac153f160c8850fa874bbb674
THeikki <heikkitormanen@luukku.com>
2022-03-12 11:39:53 +0200

    iuyt

--- src/components/alert.js
+++ src/components/alert.js
@@ -30,7 +30,7 @@ const Alert = ({ onDelete, alert }) => {
                 </thead>
                     
             </table>    
-            <a target="_blank">
+            <a target="_blank" href={`data:image/jpg;base64,${alert.picture}`}>
                 <img className="picture" src={`data:image/jpg;base64,${alert.picture}`} alt=""/>
             </a>
             
